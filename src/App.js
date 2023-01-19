31e6a5dabdedb0c99c4b5ed90f011da38ea1d302
Ivanna Zonbiv <ivannazobniv333@gmail.com>
2023-01-19 22:39:59 +0200

    finished lesson1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import {Simpsons} from "./components/Simpsons/Simpsons";
+
+import {RickMortys} from "./components/RickMortys/RickMortys";
 
 const App = () => {
+
   return (
       <div>
-          App
+          <Simpsons/>
+          <RickMortys/>
+          {/*це коменти просто для мого розуміння, як змінюється.*/}
+          {/*simpsons.map(simpsons => < Simpson key={simpsons.id} simpson={simpsons}/>)*/}
+          {/*simpson.map(simpson => <Simpson key={simpson.id} name={simpson.name} surname={simpson.surname} image={simpson.image}/>)*/}
+          {/*<Simpson key={1} name={'Homer'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>,*/}
+          {/*<Simpson key={2} name={'Marge'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}/>,*/}
+          {/*<Simpson key={3} name={'Bart'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}/>,*/}
+          {/*<Simpson key={4} name={'Lisa Marie'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png'}/>,*/}
+          {/*<Simpson key={5} name={'Maggie'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png'}/>*/}
       </div>
   );
 };
---
+++ src/components/Simpsons/Simpsons.js
@@ -0,0 +1,45 @@
+import {Simpson} from "../Simpson/Simpson";
+
+const Simpsons = () => {
+    const simpsons =[
+        {
+            id:1,
+            name:'Homer',
+            surname:'Simpson',
+            image:'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+        },
+        {
+            id:2,
+            name:'Marge',
+            surname:'Simpson',
+            image:'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+        },
+        {
+            id:3,
+            name:'Bart',
+            surname:'Simpson',
+            image:'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'
+        },
+        {
+            id:4,
+            name:'Lisa Marie',
+            surname:'Simpson',
+            image:'https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png'
+        },
+        {
+            id:5,
+            name:'Maggie',
+            surname:'Simpson',
+            image:'https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png'
+        },
+    ]
+    return (
+        <div>
+            {
+                simpsons.map(simpson => < Simpson key={simpson.id} simpson={simpson}/>)
+            }
+        </div>
+    );
+};
+
+export {Simpsons};
