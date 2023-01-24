45e93df5886e7d723bb585f79beecae2336ce5f1
Ivanna Zonbiv <ivannazobniv333@gmail.com>
2023-01-24 20:34:15 +0200

    lesson3 advance

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import {CarForm, Cars} from "./components";
+import {carService} from "./services";
+import {useEffect, useState} from "react";
 
 const App = () => {
-  return (
-      <div>
-          App
-      </div>
-  );
+    const [cars, setCars] = useState([]);
+    const [updateCar, setUpdateCar] = useState(null);
+
+    useEffect(() => {
+        carService.getAll().then(({data})=>setCars([...data]))
+    }, [])
+
+    return (
+        <div>
+            <CarForm setCars={setCars} updateCar={updateCar}/>
+            <hr/>
+            <Cars cars={cars} setUpdateCar={setUpdateCar}/>
+        </div>
+    );
 };
 
-export {App} ;
+export {App};
+
