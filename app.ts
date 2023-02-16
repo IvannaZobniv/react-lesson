d4fa08d1f09449214283b983725019e001d371bc
Ivanna Zonbiv <ivannazobniv333@gmail.com>
2023-02-16 19:33:23 +0200

    lesson8_typescript_hw

--- app.ts
+++ app.ts
@@ -1,117 +1,72 @@
-// type MyType = number | string[] | boolean
-// const test = (name:number):void => {
-//     return [name]
-// }
-//
-// console.log(test('Max'));
-//
-//
-// // const arr:number[] = [1,2,3,4]
-// const arr:[number, string] = [2,'2']
+import {StatusEnum} from './num/statusNum';
+// 1) створити інтерфейс на основі цього объекта:
+//     Зверніть увагу там де масиви... в них може бути багато однотипних обїектів
+interface ICore {
+    flight: number
+    core: {
+        reuse_count: number
+        status: string
+    }
+}
+
+interface IPayload {
+    payload_type: string
+    payload_mass_kg: number
+    payload_mass_lbs: number
+}
+
+interface ISpaceX {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        site_name_long: string
+    },
+    links: {
+        article_link: string;
+        video_link: string
+    },
+    rocket: {
+        rocket_name: string
+        first_stage: {
+            cores: ICore[]
+        },
+        second_stage: {
+            payloads: IPayload[]
+        }
+    }
+}
+
+// 2) протипізувати функції:
+interface IUser{
+    name:string;
+    age:number;
+    gender:string;
+}
+
+const user:IUser = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
+}
 
 
-// interface IUser<T, DATA2> {
-//     name: string;
-//     // name?: string;
-//     surname: string;
-//     age: number;
-//     status: boolean
-//     data:T
-//     data2:DATA2
-// }
-//
-// // const user: Partial<IUser> = {name:'max', status:true}
-//
-// // const getName = (user: IUser): string => {
-// //     return user.name
-// // }
-// //
-// //
-// // const user:IUser<string[]> = {status:true,age:18,name:'max',surname:'popov',data:['1,2,3,4', 's','s']}
-//
-// class User{
-//     constructor(private name: string, private age: number) {
-//     }
-// }
+function sum(a:number, b:number):number {
+    return a + b
+}
 
-// interface IShape {
-//     area: ()=> number;
-//     perimeter: () => number
-// }
-//
-// interface IGreeting {
-//     greeting: (name: string) => void;
-// }
-//
-// class Rectangle implements IGreeting, IShape {
-//     constructor(private a: number, private b: number) {
-//     }
-//
-//     greeting(name: string): void {
-//
-//     }
-//
-//
-//     area(): number {
-//         return this.a * this.b;
-//     }
-//
-//     perimeter(): number {
-//         return this.a * 2 + this.b * 2;
-//     }
-//
-// }
-//
-//
-// const rectangle = new Rectangle(1, 2);
-//
-// // rectangle.greeting()
-// // console.log(rectangle.area());
-// // console.log(rectangle.perimeter());
-//
-// class Triangle implements IShape {
-//     constructor(private a: number, private b: number, private c: number) {
-//     }
-//
-//     area(): number {
-//         return this.a * this.b * this.c;
-//     }
-//
-//     perimeter(): number {
-//         return this.a + this.b + this.c;
-//     }
-//
-// }
-//
-//
-// const shapes: IShape[] = [new Rectangle(1, 2), new Triangle(1, 2, 3), new Rectangle(4, 5)]
-//
-// for (let shape of shapes) {
-//     console.log(shape.area());
-//     console.log(shape.perimeter());
-// }
+function showSum(a:number, b:number):void {
+    console.log(a + b);
+}
 
-// const x = () => {
-//     return 15
-// }
+function incAge(someUser:IUser, inc:number) :IUser{
+    someUser.age += inc
+    return someUser
+}
 
-// type Mytype = ReturnType<typeof x>;
-//
-// const a:Mytype = 'sss'
+console.log(sum(1, 2));
+showSum(2, 3)
+incAge(user, 2)
 
-// const x1 = (): Function => {
-//     const f2 = (a:number):number => {
-//         return 2
-//     }
-//     return f2
-// }
-
-
-
-import {userService} from './services/userService';
-
-userService.getAll().then(({data})=>{
-    const {email} = data[3];
-})
-
-
+if (StatusEnum.UnAuthorization === res.status) {
+    console.log('UnAuthorization');
+}
